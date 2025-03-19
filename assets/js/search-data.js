501d75619b6e94d34ef956bfa2ca9dd262b95c4c
ShawnMiloGuo <guoshanxin@outlook.com>
2025-03-19 14:23:40 +0000

    Deploying to gh-pages from @ ShawnMiloGuo/ShawnMiloGuo.github.io@d30195317a891f6dd6e32c85c789cc8d9526d651 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,6 +42,13 @@ ninja.data = [{
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
+            window.location.href = "/_pages/cv/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
